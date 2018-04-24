690666118ea7db2805f57ac77e251c31dfdc2ae4
EditB <beditto@yahoo.com>
2018-04-24 18:25:20 +1000

    This is the first verison of the Free Code Camp Leaderboard project

---
+++ src/Components/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Leaderboard extends Component {
+  render() {
+    return(
+      <div>Component test</div>
+    );
+  }
+}
+
+export default Leaderboard;
