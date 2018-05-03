84c2293f8861001eb6e136f67f422646857e14e0
EditB <beditto@yahoo.com>
2018-05-03 14:55:50 +1000

    cleared up code: deleted commented out code and debugging console logs

--- src/Components/LeaderBoard/index.js
+++ src/Components/LeaderBoard/index.js
@@ -17,48 +17,20 @@ export class Leaderboard extends Component {
 
   fetchResults(){
 
-    console.log("in fectchResults")
-
     fetch('https://fcctop100.herokuapp.com/api/fccusers/top/recent')
-
       .then(resp => resp.json())
       .then(resp => {
-        console.log("raw response: ")
-        console.log(JSON.stringify(resp));
         this.setState({leaders: resp});
-    //  this.setState({leaders: [{"username":"Smootimus","img":"https://avatars3.githubusercontent.com/u/6472304?v=4","alltime":95,"recent":81,"lastUpdate":"2018-03-19T19:24:02.627Z"},{"username":"sjames1958gm","img":"https://avatars1.githubusercontent.com/u/4639625?v=4","alltime":8892,"recent":75,"lastUpdate":"2018-05-01T23:55:49.944Z"},{"username":"khaduch","img":"https://avatars2.githubusercontent.com/u/1930584?v=4","alltime":3589,"recent":59,"lastUpdate":"2018-05-01T23:56:03.667Z"}]});
-      //  console.log({this.state.leaders});
       })
-
-
-
-      /*
-        function(response){
-          return response.json();
-        }
-        ).then(function(jsonData){
-          //handle json data processing here
-          //this.setState({leaders:leaders});
-        }
-
-      );   */
-
-
   }
 
   componentDidMount() {
-    console.log("in didmount")
     this.fetchResults();
-
   }
 
   render() {
-
-    //const {leaders:leaders}=this.state;
     const {leaders} = this.state;
-  //  const leaders = [{"username":"Smootimus","img":"https://avatars3.githubusercontent.com/u/6472304?v=4","alltime":95,"recent":81,"lastUpdate":"2018-03-19T19:24:02.627Z"},{"username":"sjames1958gm","img":"https://avatars1.githubusercontent.com/u/4639625?v=4","alltime":8892,"recent":75,"lastUpdate":"2018-05-01T23:55:49.944Z"},{"username":"khaduch","img":"https://avatars2.githubusercontent.com/u/1930584?v=4","alltime":3589,"recent":59,"lastUpdate":"2018-05-01T23:56:03.667Z"}];
 
-    console.log("leaders: " + leaders);
     return (
     <div className="container">
       <LeaderHeader />
@@ -73,14 +45,7 @@ export class Leaderboard extends Component {
       ))}
     </div>
     )
-
   }
-
-
-
 }
-
-
-
 
 export default Leaderboard;
